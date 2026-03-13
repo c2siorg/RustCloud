3738bd0204ccf7544477c941ec64a3a4e1a325f0
shivv23 <shivamkumar0423@gmail.com>
2026-03-14 02:10:19 +0530

    test: gate live cloud suites behind opt-in cargo features

--- rustcloud/src/tests/mod.rs
+++ rustcloud/src/tests/mod.rs
@@ -1,22 +1,46 @@
+#[cfg(feature = "live-tests-azure")]
 mod azure_blob_operations;
+
+#[cfg(feature = "live-tests-aws")]
 mod aws_archival_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_block_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_bucket_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_dns_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_dynamodb_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_ec2_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_ecs_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_eks_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_iam_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_kms_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_loadbalancer_operations;
+#[cfg(feature = "live-tests-aws")]
 mod aws_monitoring_operations;
+
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_automl_operations;
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_bigtable_operations;
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_bigquery_operations;
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_compute_operations;
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_dns_operations;
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_kubernetes_operations;
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_loadbalancer_operations;
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_notification_operations;
+#[cfg(feature = "live-tests-gcp")]
 mod gcp_storage_operations;
